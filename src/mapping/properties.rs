37e98c29b3a10f8b990f72f4293978b46d055eec
uiuifree <info@uiuifree.com>
2023-01-02 16:01:34 +0900

    mapping

--- src/mapping/properties.rs
+++ src/mapping/properties.rs
@@ -2,24 +2,24 @@ use std::collections::HashMap;
 use serde_json::Value;
 use crate::mapping::MappingTrait;
 use crate::util::UtilMap;
+
 #[derive(Default)]
 pub struct MappingProperties {
-     properties: HashMap<String, Box<dyn MappingTrait>>,
+    properties: HashMap<String, Box<dyn MappingTrait>>,
 }
 
 impl MappingProperties {
     pub(crate) fn new() -> Self where Self: Sized {
         MappingProperties::default()
     }
-    pub fn add_property<T>(&mut self, key: &str, value: T)
-        where T: MappingTrait  + 'static{
+    pub fn add_property<T>(&mut self, key: &str, value: T) -> &mut MappingProperties
+        where T: MappingTrait + 'static {
         self.properties.insert(key.to_string(), Box::new(value));
+        self
     }
 }
 
 impl MappingTrait for MappingProperties {
-
-
     fn build(&self) -> Value {
         let mut map = UtilMap::new();
         for (k, v) in &self.properties {
